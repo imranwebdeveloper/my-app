e25928e77bff51c7ac89dd7b401efce2cfc31c36
Imran Nazir <imran.web.developer2021@gmail.com>
2023-01-02 22:33:54 +0600

    added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import Header from "../Components/Header/Header";
+import { HiMenuAlt1 } from "react-icons/hi";
+import { BiLogIn } from "react-icons/bi";
+import List from "../Components/Header/List";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -14,110 +18,39 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <main>
+        <div className="bg-white  border border-gray-200 ">
+          <div className="max-w-[1200px] mx-auto p-0 ">
+            <div className="navbar flex justify-between bg-base-100 p-0">
+              <div className="">
+                <div className="dropdown flex ">
+                  <ul className="menu-compact dropdown-content mt-8 p-2 shadow bg-base-100 rounded-box w-52 text-md font-bold font-specially uppercase">
+                    {/* {menuItems} */}
+                  </ul>
+                  <label className=" lg:hidden md:hidden">
+                    {/* <HiMenuAlt1 className="text-3xl bg-accent text-white font-bold p-1  rounded"></HiMenuAlt1> */}
+                  </label>
+                </div>
+
+                {/* Logo Insert Here */}
+                <div className="md:ml-0 ml-5">
+                  <Link href="/" className="  flex items-center">
+                    <h1 className="   md:text-2xl sm:text-xl text-lg font-bold text-primary  ">
+                      Creative University
+                    </h1>
+                  </Link>
+                </div>
+              </div>
+              <div className=" hidden lg:flex md:flex">
+                <ul className="lg-menu menu-horizontal flex items-center p-0 font-semibold   text-primary space-x-5">
+                  <List />
+                  <button className="btn btn-primary">test</button>
+                </ul>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
         </div>
       </main>
     </>
-  )
+  );
 }
